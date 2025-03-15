2048f5c275d967b258318f7553a5bacc8c52328f
Artur <ziembaart@gmail.com>
2025-03-15 20:46:48 +0100

    Delete src/.DS_Store
    Delete .DS_Store
    
    init

--- src/helpers/time.js
+++
@@ -1,9 +0,0 @@
-exports.isLastFridayOfNovember = (date) => {
-  const year = date.getFullYear();
-  if (date.getMonth() !== 10) return false;
-
-  const lastFriday = new Date(year, 11, 0);
-  lastFriday.setDate(lastFriday.getDate() - ((lastFriday.getDay() + 2) % 7));
-
-  return date.toDateString() === lastFriday.toDateString();
-};
